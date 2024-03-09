0a49e7307d2f53e20b7d75ca2ccefc960e21a907
Jashanpreet15 <200534158@student.georgianc.on.ca>
2024-03-09 16:00:16 -0600

    lab03

---
+++ LAB03/server.js
@@ -0,0 +1,45 @@
+const connect = require('connect');
+const URL = require('url');
+
+function calculate(url) {
+    const parsedUrl = URL.parse(url, true);
+    const method = parsedUrl.query.method;
+    const x = parseFloat(parsedUrl.query.x);
+    const y = parseFloat(parsedUrl.query.y);
+  
+    let result;
+    let operation;
+  
+    switch (method) {
+      case 'add':
+        result = x + y;
+        operation = '+';
+        break;
+      case 'subtract':
+        result = x - y;
+        operation = '-';
+        break;
+      case 'multiply':
+        result = x * y;
+        operation = '*';
+        break;
+      case 'divide':
+        result = x / y;
+        operation = '/';
+        break;
+      default:
+        return 'Invalid method';
+    }
+  
+    return `${x} ${operation} ${y} = ${result}`;
+  }
+  const app = connect();
+
+app.use((req, res) => {
+  const result = calculate(req.url);
+  res.end(result);
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
